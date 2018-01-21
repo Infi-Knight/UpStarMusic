3a25bff704954d93783ef636b38366ca644fbd9b
Infi-Knight <soniravi829@gmail.com>
2018-01-21 18:20:21 +0530

    Add query age range filter

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  // any subsequent .then on minQuery will be called with age
+  const minQuery = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  const maxQuery = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then(artists => artists[0].age);
+
+  // es6 implicit objec return
+  // result will be resolved with ages min from minQuery and
+  // max from maxQuery
+  return Promise.all([minQuery, maxQuery]).then(result => ({
+    min: result[0],
+    max: result[1]
+  }));
 };
